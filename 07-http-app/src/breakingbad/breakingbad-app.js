3f349b98a173d7aab4c5df4f20aa7357bc278996
Edwin Alexander <dwine29@gmail.com>
2023-06-05 15:25:31 -0500

    finalización sección doce curso js

---
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,42 @@
+/**
+ * @returns {Promise<Object>} quote
+ */
+const fetchQuote = async () => {
+  const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+  // console.log(res);
+  const data = await res.json();
+  // console.log(data);
+  return data[0];
+};
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const BreakingbadApp = async (element) => {
+  document.querySelector('#app-title').innerHTML = 'Breakingbad App';
+  //   element.innerHTML = 'Loading...';
+  // const quote = await fetchQuote();
+
+  const quoteLabel = document.createElement('blockquote');
+  const authorLabel = document.createElement('h3');
+  const nextQuoteButton = document.createElement('button');
+  nextQuoteButton.innerText = 'Next Quote';
+
+  const renderQuote = (data) => {
+    console.log('data', data);
+    quoteLabel.innerHTML = data.quote;
+    authorLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+  };
+
+  nextQuoteButton.addEventListener('click', async () => {
+    element.innerHTML = 'Loading...';
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+
+  fetchQuote().then(renderQuote);
+
+  // console.log(quote);
+};
